fee7ed8f83798f0a4c34814a85cff9149439f75b
aryastark-beep <58858681+aryastark-beep@users.noreply.github.com>
2020-05-16 15:25:04 -0400

    Updated Files

---
+++ app.js
@@ -0,0 +1,10 @@
+function active (){
+  var searchBar = document.getElementById("search");
+
+  if (searchBar.value == "search"){
+    searchBar.value ="";
+    searchBar.placeholder = Search;
+
+  }
+
+}
